0a598d908af0090145668abb10072aec684aab47
andre91998 <andremedeiros1998@gmail.com>
2025-02-03 21:00:28 -0800

    [add] test for connection to broker

---
+++ DaVinciRuntime/tests/connectToBrokerTest.py
@@ -0,0 +1,68 @@
+#################################
+# confirm that an MQTT Client can
+# connect to the HiveMQ mqtt broker
+#################################
+# INTEGRATION
+#################################
+
+import paho.mqtt.client as mqtt
+import time
+import sys
+
+def main():
+    global clientConnected
+    clientConnected = False
+
+    # Define the MQTT broker details
+    broker = "localhost"
+    port = 1883
+    topic = "your/test/topic"
+
+    # Set max test wait time (seconds)
+    duration = 10
+
+    # Callback function when the client connects to the broker
+    def on_connect(client, userdata, flags, rc):
+        global clientConnected
+        if rc == 0:
+            print("Connected to broker")
+            client.subscribe(topic)
+            clientConnected = True
+            print("MQTT Sensor Enablement Test: PASSED!")
+        else:
+            print(f"Connection failed with code {rc}")
+
+    # Create an MQTT client instance
+    client = mqtt.Client()
+
+    # Assign the callback functions
+    client.on_connect = on_connect
+
+    # Connect to the broker
+    client.connect(broker, port, 60)
+
+# Start the loop to process received messages
+    # Start the loop
+    start_time = time.time()
+    client.loop_start()
+
+    while not clientConnected:
+        if time.time() - start_time < duration:
+            pass
+        else:
+            break
+
+    if not clientConnected:
+        print("MQTT Sensor Enablement Test: FAILED!")
+
+    # Stop the loop
+    client.loop_stop()
+    if clientConnected:
+        return_code = 1
+    else:
+        return_code = 0
+    
+    sys.exit(return_code)
+
+if __name__ == "__main__":
+    main()
