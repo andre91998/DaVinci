1fc6ffbdf9eb89ae48646a0a4a73d80eb81525c8
André Medeiros <andremedeiros1998@gmail.com>
2025-03-09 20:35:20 -0700

    cleanup logs, comments, variable naming

--- DaVinciHub/app/src/main/java/com/daVinci/hub/MainActivity.java
+++ DaVinciHub/app/src/main/java/com/daVinci/hub/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
   private RecyclerView mSensorListView;
   private RPC_SensorArray mSensorList;
-  private SensorRecyclerViewAdapter adapter;
+  private SensorRecyclerViewAdapter mAdapter;
   private GrpcClient grpcClient;
   private ExecutorService mExecutor;
 
@@ -66,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
     mSensorListView.setLayoutManager(new LinearLayoutManager(this));
     mSensorList = RPC_SensorArray.newBuilder().build();
 
-    adapter = new SensorRecyclerViewAdapter(mSensorList); // Initialize adapter with mScopeList
-    mSensorListView.setAdapter(adapter);
+    mAdapter = new SensorRecyclerViewAdapter(mSensorList); // Initialize adapter with mScopeList
+    mSensorListView.setAdapter(mAdapter);
 
     //Get all sensor types via GRPC
     List<String> sensorTypes = new ArrayList<>();
@@ -92,16 +92,12 @@ public class MainActivity extends AppCompatActivity {
       sensorTypeSpinner.setEnabled(isChecked);
       sensorTypeSpinner.setActivated(isChecked);
       if (!isChecked) {
-        //TODO: Logic for show all sensors in system
         updateRecyclerView(mSensorList);
       } else {
-        //TODO: Logic for show filtered sensor list
         String selectedSensorType = (String) sensorTypeSpinner.getSelectedItem();
         updateRecyclerView(filterSensorsByType(selectedSensorType));
       }
     });
-
-    //TODO: Add on event callback for sensor type spinner?
 
     // Set up the RecyclerView
     setupRecyclerView();
@@ -109,12 +105,10 @@ public class MainActivity extends AppCompatActivity {
 
   /**
    * Method for filtering the sensor list based sensor type
-   * @param type
+   * @param type sensor type
    * @return filtered list of scopes
    */
   private RPC_SensorArray filterSensorsByType(String type) {
-    //TODO: add filtering logic will require string mapping
-    Log.d(TAG, "type: " + type);
     return RPC_SensorArray.newBuilder().addAllRPCSensor(mSensorList.getRPCSensorList().stream()
             .filter(s -> s.getSensorType().equalsIgnoreCase(type))
             .collect(Collectors.toList())).build();
@@ -125,10 +119,9 @@ public class MainActivity extends AppCompatActivity {
    * @param filteredSensors
    */
   private void updateRecyclerView(RPC_SensorArray filteredSensors) {
-    SensorRecyclerViewAdapter mAdapter = new SensorRecyclerViewAdapter(filteredSensors);
+    mAdapter = new SensorRecyclerViewAdapter(filteredSensors);
     mSensorListView.setAdapter(mAdapter);
     mSensorListView.setLayoutManager(new LinearLayoutManager(this));
-    //adapter.updateData(filteredScopes); // Create an `updateData` method in your adapter to refresh the list.
   }
 
   @Override
@@ -153,7 +146,6 @@ public class MainActivity extends AppCompatActivity {
     Future<RPC_SensorArray> sensorsFuture = mExecutor.submit(sensorsCallable);
     try {
       mSensorList = sensorsFuture.get();
-      Log.d(TAG, mSensorList.toString());
     } catch (ExecutionException | InterruptedException e) {
       e.printStackTrace();
     }
@@ -222,71 +214,4 @@ public class MainActivity extends AppCompatActivity {
       return grpcClient.getAllSensors();
     }
   }
-
-
-
-
-//  private Button sendButton;
-//  private TextView dimmerText, temperatureText, plugText;
-//
-//
-//  @Override
-//  protected void onCreate(Bundle savedInstanceState) {
-//    super.onCreate(savedInstanceState);
-//    setContentView(R.layout.activity_main);
-
-//
-//    GetDimmerDataCallable callable1 = new GetDimmerDataCallable();
-//    Future<RPC_DimmerDataArray> dimmerFuture = executor.submit(callable1);
-//
-//    GetPlugDataCallable callable2 = new GetPlugDataCallable();
-//    Future<RPC_PlugDataArray> plugFuture = executor.submit(callable2);
-//
-//    GetTemperatureDataCallable callable3 = new GetTemperatureDataCallable();
-//    Future<RPC_TemperatureDataArray> temperatureFuture = executor.submit(callable3);
-//
-//    GetSensorsCallable callable4 = new GetSensorsCallable();
-//    Future<RPC_Sensors> sensorsFuture = executor.submit(callable4);
-//
-//    try {
-//      String result = future.get();
-//      System.out.println("Result from thread: " + result);
-//
-//      //If tables are empty, this crashes
-////      RPC_DimmerDataArray dimmerDataArray = dimmerFuture.get();
-////      if (dimmerDataArray != null) {
-////        dimmerText.setText(String.valueOf(dimmerDataArray.getDimmerData(dimmerDataArray.getDimmerDataCount() - 1).getBrightness()));
-////      } else {
-////        dimmerText.setText("N/A");
-////      }
-////
-////      RPC_PlugDataArray plugDataArray = plugFuture.get();
-////      if (plugDataArray != null) {
-////        plugText.setText(String.valueOf(plugDataArray.getPlugData(plugDataArray.getPlugDataCount() - 1).getPower()));
-////      } else {
-////        plugText.setText("N/A");
-////      }
-////
-////      RPC_TemperatureDataArray temperatureDataArray = temperatureFuture.get();
-////      if (temperatureDataArray != null) {
-////        temperatureText.setText(String.valueOf(temperatureDataArray.getTemperatureData(temperatureDataArray.getTemperatureDataCount() - 1).getTemperature()));
-////      } else {
-////        temperatureText.setText("N/A");
-////      }
-////
-////      RPC_Sensors sensors = sensorsFuture.get();
-////      if (sensors != null) {
-////        Log.d(TAG, sensors.getSensorNamesList().toString());
-////      }
-//    } catch (InterruptedException | ExecutionException | IndexOutOfBoundsException e) {
-//      e.printStackTrace();
-//    } finally {
-//      executor.shutdown();
-//    }
-//  }
-//
-
-//
-//
-
 }
